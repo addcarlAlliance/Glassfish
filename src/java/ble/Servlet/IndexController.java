b920cdbdf84703c7a6ad79076881515491d69d09
Joshua Lee <joshua.leeCS03@gmail.com>
2018-03-21 21:47:29 +0800

    Updated IndexController.java

--- src/java/ble/Servlet/IndexController.java
+++ src/java/ble/Servlet/IndexController.java
@@ -5,8 +5,12 @@
  */
 package ble.Servlet;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -29,19 +33,33 @@ public class IndexController extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
+        
+        String path = request.getRequestURI().substring(request.getContextPath().length());
+        path = path.replace("/","");
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
+        if("index".equalsIgnoreCase(path)){
+           File f = new File(getServletContext().getRealPath("index.ble"));
+           FileReader file = new FileReader(f);
+           BufferedReader bf = new BufferedReader(file);
+          
+           try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet IndexController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet IndexController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            String currline;
+             while ((currline = bf.readLine()) != null) {
+		out.println(currline);
+            }
+//            out.println("<!DOCTYPE html>");
+//            out.println("<html>");
+//            out.println("<head>");
+//            out.println("<title>Servlet "+path+" sds</title>");            
+//            out.println("</head>");
+//            out.println("<body>");
+//            out.println("<h1>Servlet IndexController at " + request.getContextPath() + "</h1>");
+//            out.println("</body>");
+//            out.println("</html>");
         }
+       }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
